ea541af036c5684e04a28c78c60d8453f4494028
ng0c <anitatnguyen@outlook.com>
2021-07-26 09:23:51 +0100

    initial commit

---
+++ environment/GUI/init/05-current-net.tcl
@@ -0,0 +1,3 @@
+# Port settings for ACT-R server started at 08:22:13 7/26/2021
+set actr_port 2650
+set actr_address "127.0.0.1"
---
+++ environment/GUI/init/10-userguisettings.tcl
@@ -0,0 +1,51 @@
+# this file generated when environment is closed
+# refresh . to make sure sizes are right
+
+wm deiconify .
+update
+wm withdraw .
+if {[winfo screenwidth .] != 1440 || [winfo screenheight .] != 900 || [lindex [wm maxsize .] 0] != 1440 || [lindex [wm maxsize .] 1] != 847} {
+  set size_mismatch 1
+} else {
+  set size_mismatch 0
+}
+
+if $size_mismatch {
+  set reset_window_sizes [tk_messageBox -icon warning -title "Screen resolution changed" -type yesno \
+                                         -message "The screen resolution is not the same as it was the last time the Environment was used.  Should the window positions reset to the defaults?"]
+} else { set reset_window_sizes 0}
+if {$reset_window_sizes != "yes"} {
+  set window_config(.visicon) 700x150+370+350
+  set changed_window_list(.visicon) 1
+  set window_config(.graphic_trace) 870x500+285+200
+  set changed_window_list(.graphic_trace) 1
+  set window_config(.options) 450x274+495+313
+  set changed_window_list(.options) 1
+  set window_config(.text_trace_history) 714x340+333+280
+  set changed_window_list(.text_trace_history) 1
+  set window_config(.buffers) 470x240+485+330
+  set changed_window_list(.buffers) 1
+  set window_config(.retrieval_history) 670x380+385+300
+  set changed_window_list(.retrieval_history) 1
+  set window_config(.control_panel) 235x783+8+25
+  set changed_window_list(.control_panel) 1
+  set window_config(.param_viewer) 400x330+520+300
+  set changed_window_list(.param_viewer) 1
+  set window_config(.pick_buffers) 200x340+569+269
+  set changed_window_list(.pick_buffers) 1
+  set window_config(.stepper) 500x550+470+175
+  set changed_window_list(.stepper) 1
+  set window_config(.declarative) 420x300+510+300
+  set changed_window_list(.declarative) 1
+  set window_config(.reload_response) 500x230+470+335
+  set changed_window_list(.reload_response) 1
+  set window_config(.event_queue) 800x180+320+360
+  set changed_window_list(.event_queue) 1
+  set window_config(.copyright) 1x1+520+305
+  set changed_window_list(.copyright) 1
+  set window_config(.procedural) 500x400+470+250
+  set changed_window_list(.procedural) 1
+}
+set gui_options(p_selected) #44DA22
+set gui_options(p_matched) #FCA31D
+set gui_options(p_mismatched) #E1031E
