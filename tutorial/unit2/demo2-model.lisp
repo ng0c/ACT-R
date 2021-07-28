cd99617c2aa512789fa26e6deb543c6e85d44d7c
ng0c <anitatnguyen@outlook.com>
2021-07-28 10:33:47 +0100

    WIP

--- tutorial/unit2/demo2-model.lisp
+++ tutorial/unit2/demo2-model.lisp
@@ -1,27 +1,74 @@
 
-
+; remove any models which correctly exist and return 
+; ACT-R to initial state
 (clear-all)
 
-(define-model demo2
+; define-model includes model name and all the calls to
+; ACT-R commands that will provide the inital conditions and knowledge
+(define-model demo2  
 
+; the sgp command which is used to set parameters for the model
 (sgp :seed (123456 0))
 (sgp :v t :show-focus t :trace-detail high)
-  
-(chunk-type read-letters state)
+
+; A chunk-type is a way for the modeler to specify categories for 
+; the knowledge in the model by indicating a set of slots which will 
+; be used together in the creation and testing of chunks.
+; Format: (chunk-type type-name slot-name-1 slot-name-2 ... slot-name-n)
+; Note: chunk-type are not required and is not part of the ACT-R achitecture
+; but rather a part of the software. It makes it easier to debug and create models
+(chunk-type read-letters state) 
 (chunk-type array letter)
 
+; Specification of initial chunks to be placed in the model's
+; declarative memory:
+; Note: add-dm -> command to create a set of chunks and place 
+; those into the model’s declarative memory.
+
+; "start", "attend", "respond", "done", "goal" is the name of the different chunks
+; "isa chunk" iis the optional declaration of a chunk-type to describe the chunk being created.
+; goal = chunk name, isa chunk/read-letters = chunk-type name, state = slot name, state = slot value
 (add-dm 
- (start isa chunk) (attend isa chunk)
- (respond isa chunk) (done isa chunk)
+ (start isa chunk) 
+ (attend isa chunk)
+ (respond isa chunk) 
+ (done isa chunk)
  (goal isa read-letters state start))
 
+; -- Creating productions --
+; P = production
+; find-unattended-letter = production name
+; ==> = production condition
 (P find-unattended-letter
+; specifies a pattern to match to the goal buffer
    =goal>
+      ; declarative of the set of slots which are being used in the test
       ISA         read-letters
+      ; this first slot test in the pattern has a name = state and a constant
+      ; value = start.
+      ; For this production to match, the chunk in the goal buffer must have a slot
+      ; named state which has the value start.
       state       start
  ==>
+ ; "==>" indicates the end of production condition
+
+ ; -- Production actions --
+ ; To indicate a particular action to perform with a buffer a symbol is used 
+ ; which starts with a character that indicates the action to take, followed by 
+ ; the name of a buffer, and then the character ">". That is followed by an 
+ ; optional chunk-type declaration using isa and a chunk-type name and then the 
+ ; specification of slots and values to detail the action to perform.
+
+ ; If the buffer name is prefixed with the character "+", then the action 
+ ; is a request to that buffer’s module, and we will often refer to such an 
+ ; action as a “buffer request” where buffer is the name of the buffer indicated 
+ ; e.g. a visual-location request. Any buffer request with a “+” action will also 
+ ; cause that buffer to be cleared.
    +visual-location>
       :attended    nil
+   ; If the buffer name is prefixed with the character "=" then the action 
+   ; will cause the production to immediately modify the chunk currently in 
+   ; that buffer.
    =goal>
       state       find-location
 )
@@ -46,6 +93,7 @@
       ISA         read-letters
       state       attend
    =visual>
+      ; The “=” prefix on a symbol in a production is used to indicate a variable.
       value       =letter
    ?imaginal>
       state       free
@@ -74,7 +122,10 @@
       cmd         press-key
       key         =letter
 )
-
+; -- setting the goal --
+; The goal-focus command is provided by the goal module to 
+; allow the modeler to specify a chunk to place into the goal 
+; buffer when the model starts to run.
 (goal-focus goal)
 
 )
